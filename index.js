b87fd72ad3af2e08670be0a44ed5935621213ac5
Julio Chazari <julio.chazari@gmail.com>
2021-02-22 19:29:46 -0600

    Added context

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const typeDefs = require('./graphql/schema');
+const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGOURI } = require('./config');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req })
+});
 
 mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
