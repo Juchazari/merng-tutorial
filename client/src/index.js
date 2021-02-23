fc4550dcf3e921f63dfd610d7df276da9a67e28f
Julio Chazari <julio.chazari@gmail.com>
2021-02-22 19:31:39 -0600

    Frontend react partial setup

---
+++ client/src/index.js
@@ -0,0 +1,34 @@
+import ReactDOM from 'react-dom';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import App from './App';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:5000'
+});
+
+const authLink = setContext(() => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
